8d87c9cbeae213c18eb5a34639438ad662215c8a
theseChains <rasim0103@gmail.com>
2022-11-26 20:31:08 +0300

    Implement basic application logic functions

---
+++ src/application.c
@@ -0,0 +1,49 @@
+#include "application.h"
+
+Application createApplication()
+{
+	sfVideoMode mode = { 800, 600 };
+	sfRenderWindow* window = sfRenderWindow_create(mode, "game", sfDefaultStyle, NULL);
+
+	Application app;
+	app.m_window = window;
+
+	return app;
+}
+
+void run(Application* app)
+{
+	while (sfRenderWindow_isOpen(app->m_window))
+	{
+		processInput(app);
+		update(app);
+		render(app);
+	}
+}
+
+void update(Application* app)
+{
+	/* ... */
+}
+
+void processInput(Application* app)
+{
+	sfEvent event;
+	while (sfRenderWindow_pollEvent(app->m_window, &event))
+	{
+		if (event.type == sfEvtClosed)
+			sfRenderWindow_close(app->m_window);
+
+		if (event.type == sfEvtKeyPressed && event.key.code == sfKeyEscape)
+			sfRenderWindow_close(app->m_window);
+	}
+}
+
+void render(Application* app)
+{
+	sfRenderWindow_clear(app->m_window, sfBlack);
+
+	/* ... */
+
+	sfRenderWindow_display(app->m_window);
+}
