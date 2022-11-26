ea0166792ff7f77b1c8308c82ce1e953850f610d
theseChains <rasim0103@gmail.com>
2022-11-26 20:22:36 +0300

    Add basic application interface

---
+++ src/application.h
@@ -0,0 +1,17 @@
+#ifndef APPLICATION_H
+#define APPLICATION_H
+
+#include <SFML/Graphics/RenderWindow.h>
+
+typedef struct Application
+{
+	sfRenderWindow* m_window;
+} Application;
+
+void run(Application* app);
+
+void update(Application* app);
+void processInput(Application* app);
+void render(Application* app);
+
+#endif
