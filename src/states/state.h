3970d377c7d79004399ddc8d90e3b400d70dfa81
theseChains <rasim0103@gmail.com>
2022-11-27 15:18:04 +0300

    Add abstract state functions

--- src/states/state.h
+++ src/states/state.h
@@ -2,9 +2,10 @@
 #define STATE_STRUCTURE_H
 
 #include <SFML/Graphics/RenderWindow.h>
+#include <SFML/System/Time.h>
+#include <SFML/Window/Event.h>
 
 #include "state_identifiers.h"
-#include <stdalign.h>
 
 struct StateStack;
 
@@ -17,6 +18,11 @@ typedef struct State
 {
 	Context m_context;
 	struct StateStack* m_stateStack;
+
+	// abstract functions
+	void (*draw)(void);
+	void (*update)(sfTime deltaTime);
+	void (*handleEvent)(const sfEvent* event);
 } State;
 
 State createState(Context context, struct StateStack* stateStack);
@@ -25,5 +31,7 @@ Context createContext(sfRenderWindow* window);
 void requestStackPush(State* state, enum StateId id);
 void requestStackPop(State* state);
 void requestStackClear(State* state);
+
+Context getContext(State* state);
 
 #endif
