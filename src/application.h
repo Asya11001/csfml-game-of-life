61aa9667ba5e16b7ab000c3c1ef58f428a4670e1
theseChains <rasim0103@gmail.com>
2022-11-28 19:36:39 +0300

    Integrate state logic into application

--- src/application.h
+++ src/application.h
@@ -3,14 +3,17 @@
 
 #include <SFML/Graphics/RenderWindow.h>
 
+#include "states/state_stack.h"
+
 typedef struct Application
 {
 	sfRenderWindow* m_window;
+	StateStack m_stateStack;
 } Application;
 
 void run(Application* app);
 
-static void update(Application* app);
+static void updateApp(Application* app, sfTime deltaTime);
 static void processInput(Application* app);
 static void render(Application* app);
 
